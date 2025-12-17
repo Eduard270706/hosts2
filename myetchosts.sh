f6958a0ca0604cbf642b5b040ea61d964e4e1de6
stefan-vulpoiu <vulpoiu_stefan@yahoo.com>
2025-12-17 13:11:56 +0200

    adaugare dupa modificare

--- myetchosts.sh
+++ myetchosts.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-cat /etc/hosts | while read ip nume rest
-do
-        if [[ "$ip" == \#* ]] || [[ -z "$ip" ]] || [[ "$nume" == "localhost" ]]
-        then
-                continue
-        fi
+verificare() {
+	local host=$1
+	local ip_1=$2
+	local dns_server=$3
 
- adresa_ip=$(nslookup "$nume" | grep "Address" | tail -n 1 | awk '{print $2}')
+	ip_2=$(dig @$dns_server $host +short)
 
-        if [[ -n "$adresa_ip" ]] && [[ "ip" != "$adresa_ip" ]]; then
-                echo "Bogus IP for <$nume> in /etc/hosts!"
-        fi
-done
+	if [ "$ip_2"=="$ip_1" ]; then
+		echo "[OK] $host rezolva catre $ip_1 folosind DNS $dns_server"
+	else
+		echo "[FAIL] $host: DNS-ul zice $ip_2, dar in fisier e $ip_1"
+	fi
+}
+
+DNS_SERVER="8.8.8.8"
+
+while read ip nume restul_liniei; do
+	[[ "$ip" =~ ^#.*$ ]] || [[ -z "$ip" ]] && continue
+	if [[ "$ip" == "127.0.0.1" || "$ip" == "::1" ]]; then
+		continue
+	fi
+	verificare "$nume" "$ip" "$DNS_SERVER"
+done < /etc/hosts
